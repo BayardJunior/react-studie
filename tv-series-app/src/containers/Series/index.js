9960af2b28d0b3e39709458489707f55521719c0
sobreira.b <sobreira.b@gmail.com>
2019-08-19 23:11:01 -0300

    Dinamismo na listagem e form

--- tv-series-app/src/containers/Series/index.js
+++ tv-series-app/src/containers/Series/index.js
@@ -7,17 +7,22 @@ class Series extends Component{
       series:[]
   }
 
-  //imediatemente chamado após o componente ser renderizado
-  componentDidMount(){
-    fetch('http://api.tvmaze.com/search/shows?q=Vikings')
+  onSeriesInputChange = e => {
+    fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
     .then((response) => response.json())
     //.then((response)=> console.log(response))
     .then(json => this.setState({series: json}))
-    }
+    console.log(e)
+    console.log(e.target.value)
+  }
 
   render(){
     return(
       <div>
+        The lenght of series array - {this.state.series.length }
+        <div>
+          <input type="text" onChange= {this.onSeriesInputChange} />
+        </div>
         <SeriesList list = {this.state.series}/>
       </div>
     )
